13fc40d38122b2c7caf910bad2e2ffb35551ec8d
lestece <loiceleste94@gmail.com>
2023-03-15 18:23:39 +0000

    Fix bug related to user drafts url

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('browserecipes/', views.RecipeList.as_view(), name='browse_recipes'),
     path('my_cookbook/', views.UserRecipes.as_view(), name='my_cookbook'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
-    path('drafts/', views.UserDrafts.as_view(), name='user_drafts'),
+    path('my_drafts/', views.UserDrafts.as_view(), name='drafts'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
 ]
