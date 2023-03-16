d08b1cc28c9c4b3f892b09fef46c992c833b40f4
lestece <loiceleste94@gmail.com>
2023-03-16 18:18:20 +0000

    Change comments card structure for improved visual appearance

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm, Textarea
 from .models import Recipe, Comment
 
 
@@ -24,4 +25,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        widgets = {'body': Textarea(attrs={'rows': 2})}
 
+        labels = {'body': ''}
